ce9c4ddec3e18f334e070ffc634b5660dc68954a
Fuflick <stepan_belov_2016@mail.ru>
2024-04-16 23:24:32 +0300

    DocProcedure deletion and creation

--- test app/TestApp/Controllers/DocProcedureController.cs
+++ test app/TestApp/Controllers/DocProcedureController.cs
@@ -1,33 +1,57 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using test_app.Models;
 
 namespace test_app.Controllers
 {
     public class DocProcedureController : Controller
     {
+        // GET: DocProcedure
+        public async Task<IActionResult> Index()
+        {
+            using MyDbContext dbContext = new MyDbContext();
+            var docProcedures = await dbContext.DocProcedure.ToListAsync();
+            return View(docProcedures);
+        }
+
         // GET: DocProcedure/Create
-        public IActionResult Index()
+        public IActionResult Create()
         {
             return View();
         }
 
         // POST: DocProcedure/Create
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("DocId, ProcId")] DocProcedure docProcedure)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DocId,ProcId")] DocProcedure docProcedure)
         {
             using MyDbContext dbContext = new MyDbContext();
             if (ModelState.IsValid)
             {
-                dbContext.DocProcedure.Add(docProcedure);
+                dbContext.Add(docProcedure);
                 await dbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(docProcedure);
         }
-        
+
+        // POST: DocProcedure/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int docId, int procId)
+        {
+            using MyDbContext dbContext = new MyDbContext();
+            var docProcedure = await dbContext.DocProcedure.FindAsync(docId, procId);
+            if (docProcedure == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.DocProcedure.Remove(docProcedure);
+            await dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
