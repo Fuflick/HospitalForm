7093502160d2b924317433d1889901681205cf48
Fuflick <stepan_belov_2016@mail.ru>
2024-04-30 00:33:29 +0300

    change diagnose

--- test app/TestApp/Controllers/DiagnoseController.cs
+++ test app/TestApp/Controllers/DiagnoseController.cs
@@ -1,19 +1,24 @@
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 using test_app.Models;
 
 namespace test_app.Controllers
 {
     public class DiagnoseController : Controller
     {
-        private static MyDbContext dbContext = new MyDbContext();
+        private readonly MyDbContext _context;
+
+        public DiagnoseController()
+        {
+            _context = new MyDbContext();
+        }
+
         // GET: Diagnose
         public async Task<IActionResult> Index()
         {
-            var diagnoses = await dbContext.Diagnose.ToListAsync();
-            return View(diagnoses);
+            return View(await _context.Diagnose.ToListAsync());
         }
 
         // GET: Diagnose/Create
@@ -29,27 +34,94 @@ namespace test_app.Controllers
         {
             if (ModelState.IsValid)
             {
-                dbContext.Add(diagnose);
-                await dbContext.SaveChangesAsync();
+                _context.Add(diagnose);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(diagnose);
         }
 
-        // POST: Diagnose/Delete/5
+        // GET: Diagnose/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diagnose = await _context.Diagnose.FindAsync(id);
+            if (diagnose == null)
+            {
+                return NotFound();
+            }
+            return View(diagnose);
+        }
+
+        // POST: Diagnose/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Body,Date")] Diagnose diagnose)
         {
-            var diagnose = await dbContext.Diagnose.FindAsync(id);
+            if (id != diagnose.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(diagnose);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiagnoseExists(diagnose.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diagnose);
+        }
+
+        // GET: Diagnose/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diagnose = await _context.Diagnose
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (diagnose == null)
             {
                 return NotFound();
             }
 
-            dbContext.Diagnose.Remove(diagnose);
-            await dbContext.SaveChangesAsync();
+            return View(diagnose);
+        }
+
+        // POST: Diagnose/Delete/5
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var diagnose = await _context.Diagnose.FindAsync(id);
+            _context.Diagnose.Remove(diagnose);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiagnoseExists(int id)
+        {
+            return _context.Diagnose.Any(e => e.Id == id);
         }
     }
 }
