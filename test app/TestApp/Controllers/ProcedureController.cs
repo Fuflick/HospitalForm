5c0bffd9c6c4b5e7df0b677417997b42279a65d4
Fuflick <stepan_belov_2016@mail.ru>
2024-04-30 00:26:50 +0300

    change procedure

--- test app/TestApp/Controllers/ProcedureController.cs
+++ test app/TestApp/Controllers/ProcedureController.cs
@@ -1,20 +1,24 @@
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 using test_app.Models;
 
 namespace test_app.Controllers
 {
     public class ProcedureController : Controller
     {
+        private readonly MyDbContext _context;
+
+        public ProcedureController()
+        {
+            _context = new MyDbContext();
+        }
 
         // GET: Procedure
         public async Task<IActionResult> Index()
         {
-            using MyDbContext dbContext = new MyDbContext();
-            var procedures = await dbContext.Procedure.ToListAsync();
-            return View(procedures);
+            return View(await _context.Procedure.ToListAsync());
         }
 
         // GET: Procedure/Create
@@ -30,29 +34,94 @@ namespace test_app.Controllers
         {
             if (ModelState.IsValid)
             {
-                using MyDbContext dbContext = new MyDbContext();
-                dbContext.Add(procedure);
-                await dbContext.SaveChangesAsync();
+                _context.Add(procedure);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(procedure);
         }
 
-        // POST: Procedure/Delete/5
+        // GET: Procedure/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var procedure = await _context.Procedure.FindAsync(id);
+            if (procedure == null)
+            {
+                return NotFound();
+            }
+            return View(procedure);
+        }
+
+        // POST: Procedure/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Body,Cost,Date")] Procedure procedure)
         {
-            using MyDbContext dbContext = new MyDbContext();
-            var procedure = await dbContext.Procedure.FindAsync(id);
+            if (id != procedure.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(procedure);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProcedureExists(procedure.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(procedure);
+        }
+
+        // GET: Procedure/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var procedure = await _context.Procedure
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (procedure == null)
             {
                 return NotFound();
             }
 
-            dbContext.Procedure.Remove(procedure);
-            await dbContext.SaveChangesAsync();
+            return View(procedure);
+        }
+
+        // POST: Procedure/Delete/5
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var procedure = await _context.Procedure.FindAsync(id);
+            _context.Procedure.Remove(procedure);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProcedureExists(int id)
+        {
+            return _context.Procedure.Any(e => e.Id == id);
         }
     }
 }
